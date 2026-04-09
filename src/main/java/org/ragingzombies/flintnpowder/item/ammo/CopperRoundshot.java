2e16764bb410c103b32798e62e016eca7f5ba4c6
Shnogr Fphonovič <nekitrulitetimmirom@gmail.com>
2026-04-10 01:30:02 +0300

    Update CopperRoundshot.java

--- src/main/java/org/ragingzombies/flintnpowder/item/ammo/CopperRoundshot.java
+++ src/main/java/org/ragingzombies/flintnpowder/item/ammo/CopperRoundshot.java
@@ -31,4 +31,11 @@ public class CopperRoundshot extends BaseAmmo {
 
         level.addFreshEntity(proj);
     }
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.literal(""));
+        pTooltipComponents.add(Component.translatable("flintnpowder.bullet_description"));
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
 }
