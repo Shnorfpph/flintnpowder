f5363b727b1536776e69cd1c19d89840dafe1f11
Shnogr Fphonovič <nekitrulitetimmirom@gmail.com>
2026-04-10 01:29:42 +0300

    Update CastIronRoundshot.java

--- src/main/java/org/ragingzombies/flintnpowder/item/ammo/CastIronRoundshot.java
+++ src/main/java/org/ragingzombies/flintnpowder/item/ammo/CastIronRoundshot.java
@@ -40,4 +40,11 @@ public class CastIronRoundshot extends BaseAmmo {
 
         level.addFreshEntity(proj);
     }
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.literal(""));
+        pTooltipComponents.add(Component.translatable("flintnpowder.bullet_description"));
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
 }
